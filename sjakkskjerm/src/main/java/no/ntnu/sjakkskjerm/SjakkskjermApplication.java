82ce327ce167663b61ce7117e5fdeb4d9532b806
Pål Nyseth <paalnyseth@gmail.com>
2021-03-10 13:08:33 +0100

    init backend
    init backend w/ docker

---
+++ sjakkskjerm/src/main/java/no/ntnu/sjakkskjerm/SjakkskjermApplication.java
@@ -0,0 +1,15 @@
+package no.ntnu.sjakkskjerm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjakkskjermApplication {
+
+	// Kun en testklasse for å teste TomCat/Vue
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjakkskjermApplication.class, args);
+	}
+
+}
---
+++ sjakkskjerm/src/test/java/no/ntnu/sjakkskjerm/SjakkskjermApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.sjakkskjerm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjakkskjermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
