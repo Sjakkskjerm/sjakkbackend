7fd4b0ad863d2b25cef14564d3ef52419b0a2596
Pål Nyseth <paalnyseth@gmail.com>
2021-03-17 14:49:38 +0100

    Messages and database changes
    Works with local postgres DB 
    Messages are working (Send/See)

---
+++ sjakkskjerm/src/main/java/no/ntnu/sjakkskjerm/message/MessageController.java
@@ -0,0 +1,31 @@
+package no.ntnu.sjakkskjerm.message;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "api/message")
+public class MessageController {
+
+    private final MessageService messageService;
+
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @GetMapping
+    public List<Message> getMessages() {
+        return messageService.getMessage();
+    }
+
+    @PostMapping
+    public void sendMessage(@RequestBody Message message) {
+        messageService.sendMessage(message);
+    }
+
+}
---
+++ sjakkskjerm/src/main/java/no/ntnu/sjakkskjerm/message/MessageRepository.java
@@ -0,0 +1,13 @@
+package no.ntnu.sjakkskjerm.message;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    
+}
