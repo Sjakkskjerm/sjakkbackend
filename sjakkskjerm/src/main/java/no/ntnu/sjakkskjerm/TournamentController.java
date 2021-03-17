f0d5d43731fd5e5cae7e51da6266b0fe7cda99bf
Lasse Kregnes Hansen <lasse.2011@hotmail.com>
2021-03-17 12:44:34 +0100

    Added mapping and RESTcontroller tags

--- sjakkskjerm/src/main/java/no/ntnu/sjakkskjerm/TournamentController.java
+++ sjakkskjerm/src/main/java/no/ntnu/sjakkskjerm/TournamentController.java
@@ -1,2 +1,16 @@
-package no.ntnu.sjakkskjerm;public class TournamentController {
+package no.ntnu.sjakkskjerm;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+/*
+ * REST controller class
+ */
+@RestController
+@RequestMapping(path = "/tournaments")
+public class TournamentController {
+    
 }
