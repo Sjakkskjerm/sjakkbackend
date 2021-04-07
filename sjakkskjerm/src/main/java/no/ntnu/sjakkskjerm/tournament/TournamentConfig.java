4bc5d20bbdffd15c6489689b3319c18905ca703f
Lasse KH <lasse.2011@hotmail.com>
2021-04-07 14:23:21 +0200

    Added more test tournaments

--- sjakkskjerm/src/main/java/no/ntnu/sjakkskjerm/tournament/TournamentConfig.java
+++ sjakkskjerm/src/main/java/no/ntnu/sjakkskjerm/tournament/TournamentConfig.java
@@ -9,8 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import java.time.LocalDate;
 import java.util.List;
 
-import static java.time.Month.APRIL;
-import static java.time.Month.JANUARY;
+import static java.time.Month.*;
 
 @Configuration
 public class TournamentConfig {
@@ -20,16 +19,31 @@ public class TournamentConfig {
         return args -> {
             Tournament testTournament = new Tournament(
                    1L,
-                    LocalDate.of(2021, APRIL, 10),
+                    LocalDate.of(2021, APRIL, 5),
                     LocalDate.of(2021, APRIL, 20),
                     5,
                     4,
                     "Ola Normann"
             );
-
+            Tournament testTournament2 = new Tournament(
+                    2L,
+                    LocalDate.of(2021, JUNE, 5),
+                    LocalDate.of(2021, JUNE, 15),
+                    6,
+                    3,
+                    "Per Atle Steinar"
+            );
+            Tournament testTournament3 = new Tournament(
+                    3L,
+                    LocalDate.of(2021, DECEMBER, 25),
+                    LocalDate.of(2022, JANUARY, 5),
+                    9,
+                    2,
+                    "Julenissen"
+            );
 
             repository.saveAll(
-                    List.of(testTournament)
+                    List.of(testTournament, testTournament2, testTournament3)
             );
         };
     }
